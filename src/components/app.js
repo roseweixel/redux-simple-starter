f6ffb11ff04306a10a78cfb222bd26e2c5229633
Rose Weixel <rose.weixel@gmail.com>
2016-02-08 15:40:29 -0500

    some work on book app

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './books-reducer';
+import ActiveBook from './active-book-reducer';
 
+// Whatever is in here gets added to the global app state
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
